5540fef1083c2cbf243be9247d021b91b9ad4f07
kingveee <vivianpham22@gmail.com>
2017-07-24 14:17:27 -0400

    added book and category models

---
+++ Bookshelf/Models/Book.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace Bookshelf.Models
+{
+    public class Book
+    {
+        [Key] // Primary key
+        public int ID { get; set; } // Always want an ID when building a model
+        public string Title { get; set; }
+        public DateTime PublishedDate { get; set; }
+        public string Description { get; set; }
+        public string ISBN { get; set; }
+
+
+        [ForeignKey("Category")] // Established relationship with another class or table 
+        public int CategoryID { get; set;}
+        public virtual Category Category { get; set; } // Navigation property is the first word
+
+
+    }
+}
---
+++ Bookshelf/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Bookshelf.Models
+{
+    public class Category
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<Book> Books { get; set; } // 
+
+    }
+}
