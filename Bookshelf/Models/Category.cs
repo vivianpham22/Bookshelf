749b6c4dbff46451bc250ae06487a79096912a5e
kingveee <vivianpham22@gmail.com>
2017-07-24 15:00:45 -0400

    added controllers and views

--- Bookshelf/Models/Category.cs
+++ Bookshelf/Models/Category.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 
 namespace Bookshelf.Models
@@ -11,8 +12,11 @@ namespace Bookshelf.Models
     {
         [Key]
         public int ID { get; set; }
+
+
+        [Display(Name = "Category")]
         public string Name { get; set; }
-        public virtual ICollection<Book> Books { get; set; } // 
+        public virtual ICollection<Book> Books { get; set; } // Navigation property allows to connect with classes 
 
     }
 }
